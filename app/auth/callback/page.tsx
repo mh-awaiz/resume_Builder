c9d817bed7f4fc337b5999660040d6637d399237
Mohammed Awaiz <awaiz.web@gmail.com>
2025-09-19 20:01:41 +0530

    V5.5

--- app/auth/callback/page.tsx
+++ app/auth/callback/page.tsx
@@ -1,16 +1,16 @@
-// app/auth/callback/route.ts
-import { NextResponse } from "next/server";
-import { cookies } from "next/headers";
-import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
+// // app/auth/callback/route.ts
+// import { NextResponse } from "next/server";
+// import { cookies } from "next/headers";
+// import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
 
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const code = searchParams.get("code");
+// export async function GET(request: Request) {
+//   const { searchParams } = new URL(request.url);
+//   const code = searchParams.get("code");
 
-  if (code) {
-    const supabase = createServerActionClient({ cookies });
-    await supabase.auth.exchangeCodeForSession(code); // ✅ sets cookie properly
-  }
+//   if (code) {
+//     const supabase = createServerActionClient({ cookies });
+//     await supabase.auth.exchangeCodeForSession(code); // ✅ sets cookie properly
+//   }
 
-  return NextResponse.redirect(new URL("/dashboard", request.url));
-}
+//   return NextResponse.redirect(new URL("/dashboard", request.url));
+// }
